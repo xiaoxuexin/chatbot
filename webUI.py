9e9f09c4357044111bab8ae8c98fda7a2795c6bf
xiaoxuexin <31259898+xiaoxuexin@users.noreply.github.com>
2024-04-02 16:54:40 -0400

    Update webUI.py

--- webUI.py
+++ webUI.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
 import streamlit as st
 from openai import OpenAI
 from langchain.llms import OpenAI
