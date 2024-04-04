7dbe594942c09393d0bd0fee29e649a0d7508973
xiaoxuexin <31259898+xiaoxuexin@users.noreply.github.com>
2024-04-03 22:25:48 -0400

    Update webUI.py

--- webUI.py
+++ webUI.py
@@ -1,3 +1,7 @@
+from bs4 import BeautifulSoup
+import pathlib
+import shutil
+
 GA_ID = "google_analytics"
 GA_SCRIPT = """
 <!-- Google tag (gtag.js) -->
@@ -10,6 +14,22 @@ GA_SCRIPT = """
   gtag('config', 'G-YWTPK7QFKC');
 </script>
 """
+
+def inject_ga():    
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID): 
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  
+        else:
+            shutil.copy(index_path, bck_index)  
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
+        index_path.write_text(new_html)
+
+inject_ga()
+
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
