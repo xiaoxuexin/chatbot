02e6d6c5eec6b1c6b9b8eba70e614986b994ba00
xiaoxuexin <31259898+xiaoxuexin@users.noreply.github.com>
2024-04-03 22:58:32 -0400

    Update add_analytics.py

--- add_analytics.py
+++ add_analytics.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import pathlib
 import shutil
 import streamlit as st
+import logging
 GA_ID = "google_analytics"
 GA_SCRIPT = """
 <!-- Google tag (gtag.js) -->
@@ -17,13 +18,14 @@ GA_SCRIPT = """
 
 def inject_ga():    
     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
+    if not soup.find(id=analytics_id): # if id not found within html file
         bck_index = index_path.with_suffix('.bck')
         if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
+            shutil.copy(bck_index, index_path)  # backup recovery
         else:
-            shutil.copytree(index_path, bck_index)  
+            shutil.copy(index_path, bck_index)  # save backup
         html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
+        new_html = html.replace('<head>', '<head>\n' + analytics_js) 
         index_path.write_text(new_html)
